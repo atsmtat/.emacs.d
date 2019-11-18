6029fa40a8e2e89d7686d506a3793dcf29ceea30
Smit Soni <ssmit004@gmail.com>
2019-11-17 19:39:43 -0800

    - Use use-package to install, autoload, and configure packages

--- custom/setup-editing.el
+++ custom/setup-editing.el
@@ -1,14 +1,5 @@
 (provide 'setup-editing)
 
-;; add line numbers except no shell and compilation buffers 
-
-;; (global-linum-mode 1)
-;; (setq linum-format "%d ")
-;; (setq linum-disabled-modes-list '(eshell-mode wl-summary-mode compilation-mode shell-mode))
-;; (defun linum-on ()
-;;   (unless (or (minibufferp) (member major-mode linum-disabled-modes-list))
-;;     (linum-mode 1)))
-
 ;; Handy incremental scrolling keys
 (define-key global-map "\M-n" (lambda () (interactive) (next-line 5)))
 (define-key global-map "\M-p" (lambda () (interactive) (previous-line 5)))
@@ -43,3 +34,55 @@
   (transpose-lines 1)
   (forward-line -1))
 (global-set-key (kbd "ESC <down>") 'move-line-down)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: rebox2 ;;
+;;                 ;;
+;; GROUP: editing  ;;
+;;;;;;;;;;;;;;;;;;;;;
+(use-package rebox2
+  :ensure t
+  :init
+  (setq rebox-style-loop '(21 25 10))
+  :bind (("M-q" . rebox-dwim)
+	 ("S-M-q" . rebox-cycle)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: duplicate-thing ;;
+;;                          ;;
+;; GROUP: editing           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package duplicate-thing
+  :ensure t
+  :bind ("M-c" . duplicate-thing))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: volatile-highlights ;;
+;;                              ;;
+;; GROUP: editing               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package volatile-highlights
+  :ensure t
+  :config
+  (volatile-highlights-mode t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: undo-tree ;;
+;;                    ;;
+;; GROUP: editing     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; (use-package undo-tree
+;;   :ensure t)
+;; (global-undo-tree-mode)
+;; (global-set-key (kbd "C-x >") (kbd "C-u 8 C-x ^"))
+;; (global-set-key (kbd "C-x <") (kbd "C-u - 8 C-x ^"))
+
+
+;; (use-package cp-utils
+;;   :ensure t)
+;; (global-set-key (kbd "C-x c w") 'cp-cur-word)
+;; (global-set-key (kbd "C-x c f") 'cp-cur-file-name)
+;; (global-set-key (kbd "C-x c h") 'cp-which-func)
+;; (global-set-key (kbd "C-x c b") 'cp-which-func-and-cur-file-name)
--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; y or n 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -7,6 +6,11 @@
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
+
+;; install use-package if it's not installed already
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
 
 ;; add custom module path
 (add-to-list 'load-path "~/.emacs.d/custom/")
@@ -17,68 +21,17 @@
   (require 'setup-arista)
 );;
 (require 'setup-editing)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (misterioso)))
- '(ediff-combination-pattern
+ '(package-selected-packages
    (quote
-    (">>>> ORIGINAL" Ancestor "==== THEIRS" B "==== YOURS" A "<<<<")))
- '(indent-tabs-mode nil)
- '(package-selected-packages (quote (duplicate-thing rebox2))))
+    (cp-utils volatile-highlights use-package rebox2 duplicate-thing))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;;;;;;;;;;;;;;;;;;;;;
-;; PACKAGE: rebox2 ;;
-;;                 ;;
-;; GROUP: editing  ;;
-;;;;;;;;;;;;;;;;;;;;;
-(setq rebox-style-loop '(21 25 10))
-(require 'rebox2)
-(global-set-key [(meta q)] 'rebox-dwim)
-(global-set-key [(shift meta q)] 'rebox-cycle)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; PACKAGE: duplicate-thing ;;
-;;                          ;;
-;; GROUP: editing           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'duplicate-thing)
-(global-set-key (kbd "M-c") 'duplicate-thing)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; PACKAGE: volatile-highlights ;;
-;;                              ;;
-;; GROUP: editing               ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'volatile-highlights)
-(volatile-highlights-mode t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-;; PACKAGE: undo-tree ;;
-;;                    ;;
-;; GROUP: editing     ;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-(global-set-key (kbd "C-x >") (kbd "C-u 8 C-x ^"))
-(global-set-key (kbd "C-x <") (kbd "C-u - 8 C-x ^"))
-
-(require 'cp-utils)
-(global-set-key (kbd "C-x c w") 'cp-cur-word)
-(global-set-key (kbd "C-x c f") 'cp-cur-file-name)
-(global-set-key (kbd "C-x c h") 'cp-which-func)
-(global-set-key (kbd "C-x c b") 'cp-which-func-and-cur-file-name)
